33f7243f2038ec00c7a6a5d941e5067ee4c208d6
Maritha Miracle <marithamiracle@gmail.com>
2026-02-03 22:14:02 +0100

    Updated auth request body

--- server/src/constants/requests/auth/dev-login.request.ts
+++ server/src/constants/requests/auth/dev-login.request.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class DevLoginRequestDto {
-  @ApiProperty({ example: 'test@example.com' })
+  @ApiProperty({ example: 'marithamiracle@gmail.com' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
